17f7fbee5b13f5330dbf1d25fcfc947d8259b103
Shawn Swineford <psswineford@gmail.com>
2021-08-23 14:20:04 -0700

    JS Exercise 1-2
    Exercise 1-2 added notes to my readme
    Also added an array of pokemon to the JS file.

---
+++ README.md
@@ -0,0 +1,8 @@
+JavaScript App : Pokedex
+
+This app will be a small web application utilizing HTML, CSS, and JavaScript that loads data from an external API and enables the viewing of data points.
+
+Project Dependencies:
+  HTML
+  CSS
+  JavaScript
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello World!');
-let favoriteFood = 'Pizza!!!!';
-document.write('my favorite food is ' + favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+  },
+  {
+    name: 'Vulpix',
+    height: 0.6,
+    type: ['fire']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: ['electric']
+  }
+];
