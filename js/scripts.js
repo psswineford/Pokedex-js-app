8aed32a4f23a74a54d88ed9c4a33a3b696dee779
Shawn Swineford <psswineford@gmail.com>
2021-08-26 15:13:52 -0700

    Update for task 1.5
    Updated the Js File for task 1.5 Wrapped the pokemon array into an IIFE variable.
    -updated the loop to work with the new variable to display the output to the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,60 @@
-const pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    type: ['fairy', 'normal']
-  },
-  {
-    name: 'Vulpix',
-    height: 0.6,
-    type: ['fire']
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    type: ['electric']
-  }
-];
+//wrap pokemon list in IIFE variable
+let pokemonRepository = (function() {
+  //define pokemon array
+  const pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      type: ['fairy', 'normal']
+    },
+    {
+      name: 'Vulpix',
+      height: 0.6,
+      type: ['fire']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+    }
+  ];
+  //add pokemon function
+  function add(pokemon) {
+    if(typeof pokemon === "object"){ // checking to make sure pokemon is object before allowing add
+    pokemonList.push(pokemon);
+    }
+  };
+  //return all pokemon function
+  function getAll() {
+    return pokemonList;
+  };
 
-// loop through pokemonList array and print a list of pokemon
-for (let i = 0; i < pokemonList.length; i++){
-  let pokemonHeight = pokemonList[i].height;
-  let comment = '';  // initilizing the comment variable
-  //if else statement to set comment on pokemon based on height size
-  if (pokemonHeight > 1) {
-    comment = "Wow , what a big Pokemon!";
-  } else if (pokemonHeight < 0.5){
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//retrieve pokemon array and display into HTML
+pokemonRepository.getAll().forEach(function(pokemon) {
+  let comment = '';  //initialize comment variable
+  if (pokemon.height > 1){
+    comment = "Wow, What a big Pokemon!";
+  }else if (pokemon.height < 0.5) {
     comment = "What a tiny Pokemon!";
-  } else {
+  }else {
     comment = "That's a cute Pokemon!";
   }
-  //write out the pokemon list to html
-  document.write(`${pokemonList[i].name} - Height: ${pokemonList[i].height} -- ${comment}<br>`)
-}
+  document.write(`${pokemon.name} - Height: ${pokemon.height} -- ${comment} <br>`);
+
+});
